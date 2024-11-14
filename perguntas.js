bd7eda55eec7aab0feaa75d88fe6e3adb61c0e15
SonGabriblox <102994891+SonGabriblox@users.noreply.github.com>
2024-11-14 11:23:52 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,156 @@
+export const perguntas = [
+    {
+        enunciado: "Qual é o nome da primeira vítima de william afton?",
+        alternativas: [
+            {
+                texto: "Elizabeth",
+                afirmacao: [
+                    "1° = errada",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Charlie",
+                afirmacao: [
+                    "1° = certa",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "O'Que Frisk tem de mais?",
+        alternativas: [
+            {
+                texto: "Determinação",
+                afirmacao: [
+                    "2° = certa",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Vontade genocida",
+                afirmacao: [
+                    "2° = errada",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Aonde o sonic apareceu pela primeira vez?",
+        alternativas: [
+            {
+                texto: "Num jogo de corrida",
+                afirmacao: [
+                    "3° = certa",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "No próprio jogo dele",
+                afirmacao: [
+                    "3° = errada",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Em que ano acontece os eventos do filme Godzilla:destroy all monsters",
+        alternativas: [
+            {
+                texto: "1968",
+                afirmacao: [
+                    "4° = errada",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "1999",
+                afirmacao: [
+                    "4° = certa",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Jurassic Park foi revolucionário, pois ele utilizou...",
+        alternativas: [
+            {
+                texto: "Usou robôs gigantes de dinossauros",
+                afirmacao: [
+                    "5° = errada",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Uma computação gráfica incrivel",
+                afirmacao: [
+                    "5° = certa",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "power ranger foi criados pois...",
+        alternativas: [
+            {
+                texto: "Por queriam trazer progamas do genero super sentai nas americas",
+                afirmacao: [
+                    "6° = certa",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "por que o criador teve a ideia dentro num restaurante japones",
+                afirmacao: [
+                    "6° = errada",
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "quais eram os mascotes das empresas que tavam fazendo a guerra dos video games do anos 90?",
+        alternativas: [
+            {
+                texto: "",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
+];
